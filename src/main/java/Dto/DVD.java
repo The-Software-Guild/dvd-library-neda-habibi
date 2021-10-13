44c750c7b6d387555a6a74861e1bb5f07bda045e
Neda Habibi <nedahabibi28@gmail.com>
2021-10-13 22:06:07 +0100

    003:Add UI layer

--- src/main/java/Dto/DVD.java
+++ src/main/java/Dto/DVD.java
@@ -1,2 +1,55 @@
-package Dto;public class DVD {
+package Dto;
+import java.time.LocalDate;
+
+public class DVD {
+    private String title, rating, directorName, studio, userRating;
+    private LocalDate releaseDate;
+
+    public DVD(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
 }
