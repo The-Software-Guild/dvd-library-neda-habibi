25863486bbdcdce70cc5f7a810e005b3348c7f91
Neda Habibi <84879374+neda-habibi@users.noreply.github.com>
2021-10-13 23:28:49 +0100

    004: Problem of Dao fixed.

--- src/main/java/Dao/IDvdLibraryDao.java
+++ src/main/java/Dao/IDvdLibraryDao.java
@@ -1,2 +1,19 @@
-package Dao;public interface IDvdLibraryDao {
+package Dao;
+
+import Dto.DVD;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public interface IDvdLibraryDao {
+    void addDVD(DVD dvd) throws IOException;
+
+    boolean deleteDVD(String title) throws IOException;
+
+    boolean updateDVD(DVD dvd) throws IOException;
+
+    List<DVD> getAllDVDs() throws FileNotFoundException;
+
+    DVD getDVD(String title);
 }
